f258aeb2fada91603cf55ff148bf2643d2978828
totalitymedia <totalitymofficial@gmail.com>
2024-11-02 12:11:04 -0400

    Use user location

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,27 +1,50 @@
 import { Stack } from 'expo-router';
-import { View, Text, Image, FlatList } from 'react-native';
+import { View, Text, Image, FlatList, Alert } from 'react-native';
 import EventListItem from '~/components/EventListItem';
 
 import { supabase } from '~/utils/supabase';
 import { useEffect, useState } from 'react';
 import { NearbyEvent } from '~/types/db';
+import * as Location from 'expo-location';
 
 export default function Events() {
   const [events, setEvents] = useState<NearbyEvent[] | null>([]);
+  const [location, setLocation] = useState<Location.LocationObject | null>(null);
+  const [status, requestPermission] = Location.useForegroundPermissions();
 
   useEffect(() => {
-    fetchNearbyEvents();
-  }, []);
+    if (status && !status?.granted && status.canAskAgain) {
+      requestPermission();
+    }
+  }, [status]);
 
-  const fetchAllEvents = async () => {
-    const { data, error } = await supabase.from('events').select('*');
-    setEvents(data);
-  };
+  useEffect(() => {
+    (async () => {
+      if (!status?.granted) {
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, [status]);
+
+  useEffect(() => {
+    if (status?.granted && location) {
+      fetchNearbyEvents();
+    }
+  }, [location]);
+
+  // const fetchAllEvents = async () => {
+  //   const { data, error } = await supabase.from('events').select('*');
+  //   setEvents(data);
+  // };
 
   const fetchNearbyEvents = async () => {
+    if (!location) return;
     const { data, error } = await supabase.rpc('nearby_events', {
-      lat: 41.375,
-      long: 2.16,
+      lat: location.coords.latitude,
+      long: location.coords.longitude,
     });
     setEvents(data);
   };
